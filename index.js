1ac4bdb1e0b70ebea78c82f9abe641a6b0adc2db
Nazmul Huda <mhnazmul48@gmail.com>
2020-10-04 22:37:40 +0600

    Assignment Serverside Done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
+const ObjectId = require('mongodb').ObjectId
 
 
 app.use(cors())
@@ -14,12 +15,43 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true})
 client.connect(err => {
   const taskCollection = client.db(process.env.DB_NAME).collection("volunteer-tasks")
+  const userCollection = client.db(process.env.DB_NAME).collection("user-tasks")
 
-  app.get("/allTasks",(req,res) => {
-    taskCollection.find({})
-    .toArray((err,documnets)=>res.send(documnets))
+    app.get("/allTasks",(req,res) => {
+      taskCollection.find({})
+      .toArray((err,documnets)=>res.send(documnets))
+    })
 
-  })
+    app.post("/addEvent",(req,res)=>{
+      taskCollection.insertOne(req.body)
+      .then(result=>res.send(result))
+    })
+
+    app.get("/singleTask",(req,res) => {
+      taskCollection.find({_id:ObjectId(req.query.id)})
+      .toArray((err,documnet)=>res.send(documnet[0]))
+    })
+
+    app.post("/userTask",(req,res)=>{
+      userCollection.insertOne(req.body)
+      .then(result=>res.send(result))
+    })
+
+    app.get("/getUserTask",(req,res) => {
+      userCollection.find({email:req.query.email})
+      .toArray((err,documnets)=>res.send(documnets))
+    })
+
+    app.delete("/deleteTask/:id",(req,res)=>{
+       userCollection.deleteOne({_id:req.params.id})
+      .then(result=>res.send(result))
+    })
+
+    app.get("/allUser",(req,res) => {
+      userCollection.find({})
+      .toArray((err,documnets)=>res.send(documnets))
+    })
+
 
 })
 
